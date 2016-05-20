bbbd5e745ac3cb1974b206cfd25986dd1537c5aa
Alexander Vogelgsang <vogel91@gmx.de>
2016-05-20 17:21:37 +0200

    B05A01

--- Blatt05/ag1/pi.cpp
+++ Blatt05/ag1/pi.cpp
@@ -1,20 +1,23 @@
 #include <mpi.h>
 
 #include <boost/timer/timer.hpp>
+
+#include <cassert>
 #include <cmath>
 
 #include <algorithm>
 #include <chrono>
 #include <functional>
-#include <iomanip>
 #include <iostream>
 #include <random>
 #include <vector>
 using namespace std;
 
+#define ASSERT_10_PROCESSES 1
+
 #define DBG(str) (std::cout << (str) << std::endl)
 
-constexpr int MASTER_ID = 0;
+constexpr int MASTER_RANK = 0;
 constexpr double PI = 3.14159265359;
 
 double monte_carlo(long samples) {
@@ -39,42 +42,50 @@ int main(int argc, char *argv[]) {
   int samples = 10000;
   if (argc == 2) samples = atoi(argv[1]);
 
-  int taskid;
-  int numtasks;
+  MPI::Init(argc, argv);
+  const int numtasks = MPI::COMM_WORLD.Get_size();
+  const int world_rank = MPI::COMM_WORLD.Get_rank();
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-  MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+  if (numtasks < 2) {
+    cerr << "must specify at least 2 tasks" << endl;
+    MPI::Finalize();
+    return 1;
+  }
+#if ASSERT_10_PROCESSES
+  if (numtasks != 10) {
+    cerr << "please specify 10 tasks" << endl;
+    MPI::Finalize();
+    return 2;
+  }
+#endif
 
-  cout << "MPI task " << taskid << " started..." << endl;
+  MPI::Intracomm group_comm = MPI::COMM_WORLD.Split(world_rank % 2, 0);
+  const int group_numtasks = group_comm.Get_size();
+  const int group_rank = group_comm.Get_rank();
+
+#if ASSERT_10_PROCESSES
+  assert(("each group should contain 5 processes", group_numtasks == 5));
+#endif
+
+  cout << "MPI task " << world_rank << "," << group_rank << " started..."
+       << endl;
 
   double my_pi = monte_carlo(samples);
 
   double pi;
-  MPI_Reduce(&my_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, MASTER_ID, MPI_COMM_WORLD);
-  if (taskid == MASTER_ID) {
-    pi = pi / numtasks;
-    cout << setprecision(8) << pi << endl;
+  group_comm.Reduce(&my_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, MASTER_RANK);
+  if (group_rank == MASTER_RANK) {
+    pi = pi / group_numtasks;
+    cout << pi << endl;
     cout << "delta to PI: " << abs(PI - pi) << endl;
+    if (world_rank == MASTER_RANK) {
+      double other_pi;
+      MPI::COMM_WORLD.Recv(&other_pi, 1, MPI_DOUBLE, 1, 0);
+      cout << "delta between calculations: " << abs(pi - other_pi) << endl;
+    } else {
+      MPI_Send(&pi, 1, MPI_DOUBLE, MASTER_RANK, 0, MPI_COMM_WORLD);
+    }
   }
-
-  // if (taskid == MASTER_ID) {
-  // vector<double> pi_values;
-  // pi_values.resize(numtasks, 0);
-  // pi_values[0] = my_pi;
-  // for (int i = 1; i < numtasks; ++i) {
-  //   MPI_Recv(&pi_values[i], 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD,
-  //            MPI_STATUS_IGNORE);
-  // }
-  // for (int i = 0; i < pi_values.size(); ++i) {
-  //   cout << i << ": " << setprecision(8) << pi_values[i] << endl;
-  // }
-  // double pi = accumulate(begin(pi_values), end(pi_values), 0.0) / numtasks;
-  //   cout << "-> " << setprecision(8) << pi << endl;
-  //   cout << "delta to PI: " << abs(PI - pi) << endl;
-  // } else {
-  //   MPI_Send(&my_pi, 1, MPI_DOUBLE, MASTER_ID, 0, MPI_COMM_WORLD);
-  // }
-
-  MPI_Finalize();
+  MPI::Finalize();
+  return 0;
 }
