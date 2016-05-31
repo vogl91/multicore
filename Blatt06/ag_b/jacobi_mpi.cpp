2c68aa23ce984bdaa196aacddb09672f4f3cbaf7
Alexander Vogelgsang <vogel91@gmx.de>
2016-05-31 09:54:25 +0200

    partial b

--- Blatt06/ag_b/jacobi_mpi.cpp
+++ Blatt06/ag_b/jacobi_mpi.cpp
@@ -14,6 +14,10 @@
 #include "matrix.h"
 using Matrix_t = Matrix<double>;
 
+#define DBG(str) std::cout << (str) << std::endl;
+
+constexpr int MASTER_RANK = 0;
+
 std::ostream& operator<<(std::ostream& os, const Matrix_t& m) {
   const double max = *std::max_element(m.begin(), m.end());
   const size_t max_length = std::max(1.0, log10(max));
@@ -40,14 +44,12 @@ void fill_random(Matrix_t& m) {
 using namespace std;
 
 static double deviation(const Matrix_t& u_new, const Matrix_t& u_old) {
-  assert(u_old.width() == u_old.height());
-  assert(u_new.width() == u_new.height());
+  assert(u_old.height() == u_old.height());
   assert(u_old.width() == u_new.width());
 
-  const auto N = u_old.height() - 1;
   double diff = 0;
-  for (auto i = 1u; i < N; ++i) {
-    for (auto j = 1u; j < N; ++j) {
+  for (auto i = 1u; i < u_old.height() - 1; ++i) {
+    for (auto j = 1u; j < u_old.width() - 1; ++j) {
       diff += (u_new(i, j) - u_old(i, j)) * (u_new(i, j) - u_old(i, j));
     }
   }
@@ -67,14 +69,15 @@ Matrix_t jacobi(Matrix_t u_old, double epsilon) {
 
   const auto N = u_old.height() - 1;
   size_t loop_count = 0;
-  while (true) {
+  // while (true) {
+  for(int i=0;i<1000;++i) {
     ++loop_count;
     for (auto i = 1u; i < N; ++i) {
       for (auto j = 1u; j < N; ++j) {
         u_new(i, j) = four_cell_neighbour_sum(u_old, i, j) / 4;
       }
     }
-    if (deviation(u_new, u_old) < epsilon) break;
+    // if (deviation(u_new, u_old) < epsilon) break;
     Matrix_t& tmp = u_old;
     u_old = u_new;
     u_new = tmp;
@@ -83,31 +86,130 @@ Matrix_t jacobi(Matrix_t u_old, double epsilon) {
   return u_new;
 }
 
-void do_jacobi() {
-  boost::timer::auto_cpu_timer t;
-  const size_t N = 6;
+void exchange_upper(Matrix_t& m, int rank) {
+  if (rank > 0) {
+    MPI::COMM_WORLD.Sendrecv(
+        m.begin_row(1), m.width(), MPI_DOUBLE, rank - 1, 0,  //
+        m.begin_row(0), m.width(), MPI_DOUBLE, rank - 1, 0);
+  }
+}
+
+void exchange_lower(Matrix_t& m, int rank, int numtasks) {
+  if (rank != numtasks - 1) {
+    MPI::COMM_WORLD.Sendrecv(
+        m.begin_row(m.height() - 2), m.width(), MPI_DOUBLE, rank + 1, 0,  //
+        m.begin_row(m.height() - 1), m.width(), MPI_DOUBLE, rank + 1, 0);
+  }
+}
+
+void exchange_borders(Matrix_t& m, int rank, int numtasks) {
+  if (rank % 2 == 0) {
+    exchange_upper(m, rank);
+    exchange_lower(m, rank, numtasks);
+  } else {
+    exchange_lower(m, rank, numtasks);
+    exchange_upper(m, rank);
+  }
+}
+
+constexpr double epsilon = 0.0001;
+void do_jacobi(Matrix_t& u_old, int rank) {
+  const auto numtasks = MPI::COMM_WORLD.Get_size();
+
+  Matrix_t u_new = u_old;
+
+  // while (true) {
+  for(int i=0;i<1000;++i) {
+    exchange_borders(u_old, rank, numtasks);
+    for (auto i = 1u; i < u_new.height() - 1; ++i) {
+      for (auto j = 1u; j < u_new.width() - 1; ++j) {
+        u_new(i, j) = four_cell_neighbour_sum(u_old, i, j) / 4;
+      }
+    }
+    auto delta = deviation(u_new, u_old);
+    swap(u_old, u_new);
+    // if (delta < epsilon) break;
+  }
+}
+
+void main_master() {
+  cout << "starting master..." << endl;
+
+  const size_t N = 11;
   Matrix_t m{N, N};
   fill_random(m);
 
+  Matrix_t mcopy = m;
   cout << m << endl;
-  auto result = jacobi(m, 0.0000000001);
-  cout << result << endl;
+
+  const auto numtasks = MPI::COMM_WORLD.Get_size();
+  const auto rows_per_task = N / numtasks + (N % numtasks != 0);
+  const auto chunk_size = rows_per_task * N;
+  const auto master_rows = N - (numtasks - 1) * rows_per_task;
+  const auto master_chunk_size = master_rows * N;
+
+  {  // send chunk sizes
+    int buffer[2] = {N, static_cast<int>(rows_per_task)};
+    MPI::COMM_WORLD.Bcast(&buffer, 2, MPI_INT, MASTER_RANK);
+  }
+  // send matrix data
+  for (auto i = 1; i < numtasks; ++i) {
+    auto data = m.begin_row(master_rows + (i - 1) * rows_per_task);
+    MPI::COMM_WORLD.Send(data, chunk_size, MPI_DOUBLE, i, 0);
+  }
+
+  {  // calculate
+    Matrix_t m2{master_rows + 1, N};
+    std::copy(m.begin_row(0), m.begin_row(0) + master_chunk_size, m2.begin());
+    do_jacobi(m2, MASTER_RANK);
+    std::copy(m2.begin(), m2.end(), m.begin());
+  }
+
+  // receive result data
+  for (auto i = 1; i < numtasks; ++i) {
+    auto data = m.begin_row(master_rows + (i - 1) * rows_per_task);
+    MPI::COMM_WORLD.Recv(data, chunk_size, MPI_DOUBLE, i, 0);
+  }
+
+  cout << m << endl;
+
+  mcopy = jacobi(mcopy,0.0001);
+  cout << (m == mcopy) << endl;
 }
 
-void test_swap() {
-  Matrix_t m{2, 3};
-  Matrix_t n{3, 2};
-  fill_random(m);
-  fill_random(n);
-  cout << m << endl;
-  cout << n << endl;
-  swap(m, n);
-  cout << m << endl;
-  cout << n << endl;
-}
+void main_slave(int rank) {
+  cout << "starting slave " << rank << "..." << endl;
 
+  const auto numtasks = MPI::COMM_WORLD.Get_size();
+  size_t N, rows_per_task;
+  {
+    int buffer[2];
+    MPI::COMM_WORLD.Bcast(&buffer, 2, MPI_INT, MASTER_RANK);
+    N = buffer[0];
+    rows_per_task = buffer[1];
+  }
+  const auto is_last = rank == numtasks - 1;
+  const auto chunk_size = rows_per_task * N;
+  Matrix_t m{rows_per_task + (is_last ? 1 : 2), N};
+  MPI::COMM_WORLD.Recv(m.begin_row(1), chunk_size, MPI_DOUBLE, MASTER_RANK, 0);
+
+  do_jacobi(m, rank);
+
+  MPI::COMM_WORLD.Send(m.begin_row(1), chunk_size, MPI_DOUBLE, MASTER_RANK, 0);
+  cout << "shutting down slave " << rank << "..." << endl;
+}
 int main(int argc, char* argv[]) {
-  do_jacobi();
+  boost::timer::auto_cpu_timer t;
+  MPI::Init(argc, argv);
 
+  const int rank = MPI::COMM_WORLD.Get_rank();
+
+  if (rank == MASTER_RANK) {
+    main_master();
+  } else {
+    main_slave(rank);
+  }
+
+  MPI::Finalize();
   return 0;
 }
--- Blatt06/ag_b/matrix.h
+++ Blatt06/ag_b/matrix.h
@@ -1,11 +1,12 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <cassert>
 #include <cstddef>
 
 #include <memory>
 
-template<typename T>
+template <typename T>
 class Matrix;
 
 template <typename T>
@@ -20,11 +21,21 @@ class Matrix {
   Matrix(size_t height, size_t width)
       : height_(height), width_(width), data_{new value_t[height * width]} {}
   Matrix(const Matrix& that) : Matrix{that.height(), that.width()} {
-    copy_data(that);
+    copy_data(that.data());
   }
   Matrix(Matrix&&) = default;
+  Matrix(size_t height, size_t width, const T* data) : Matrix{height, width} {
+    copy_data(data);
+  }
+
   Matrix& operator=(const Matrix& that) {
-    copy_data(that);
+    if (height() != that.height() || width() != that.width()) {
+      height_ = that.height();
+      width_ = that.width();
+      data_ =
+          std::unique_ptr<value_t[]>{new value_t[that.height() * that.width()]};
+    }
+    copy_data(that.data());
     return *this;
   }
   Matrix& operator=(Matrix&&) = default;
@@ -34,14 +45,41 @@ class Matrix {
   size_t width() const { return width_; }
   value_t* data() { return data_.get(); }
   const value_t* data() const { return data_.get(); }
-  value_t& get(size_t i, size_t j) { return data_[index(i, j)]; }
-  const value_t& get(size_t i, size_t j) const { return data_[index(i, j)]; }
+  value_t& get(size_t i, size_t j) {
+    assert(i < height());
+    assert(j < width());
+    return data_[index(i, j)];
+  }
+  const value_t& get(size_t i, size_t j) const {
+    assert(i < height());
+    assert(j < width());
+    return data_[index(i, j)];
+  }
   value_t& operator()(size_t i, size_t j) { return get(i, j); }
   const value_t& operator()(size_t i, size_t j) const { return get(i, j); }
   value_t* begin() { return data(); }
   const value_t* begin() const { return data(); }
   value_t* end() { return data() + height() * width(); }
   const value_t* end() const { return data() + height() * width(); }
+
+  value_t* begin_row(size_t i) {
+    assert(i < height());
+    return data() + i * width();
+  }
+  const value_t* begin_row(size_t i) const {
+    assert(i < height());
+    return data() + i * width();
+  }
+  value_t* end_row(size_t i) {
+    assert(i >= 0);
+    assert(i < height());
+    return data() + (i + 1) * width() - 1;
+  }
+  const value_t* end_row(size_t i) const {
+    assert(i >= 0);
+    assert(i < height());
+    return data() + (i + 1) * width() - 1;
+  }
 
  public:
   friend void swap<T>(Matrix& m, Matrix& n);
@@ -53,16 +91,30 @@ class Matrix {
 
  private:
   size_t index(size_t i, size_t j) const { return i * width_ + j; }
-  void copy_data(const Matrix& that) {
-    std::copy(that.data(), that.data() + height() * width(), data());
+  void copy_data(const T* data) {
+    std::copy(data, data + height() * width(), this->data());
   }
 };
 
 template <typename T>
 void swap(Matrix<T>& m, Matrix<T>& n) {
-  std::swap(m.height_,n.height_);
-  std::swap(m.width_,n.width_);
+  std::swap(m.height_, n.height_);
+  std::swap(m.width_, n.width_);
   std::swap(m.data_, n.data_);
+}
+
+template <typename T>
+bool operator==(const Matrix<T>& lhs, const Matrix<T>& rhs) {
+  if (lhs.height() != rhs.height()) return false;
+  if (lhs.width() != rhs.width())
+    return false;
+  else
+    return std::equal(lhs.begin(), lhs.end(), rhs.begin(), lhs.end());
+}
+
+template <typename T>
+bool operator!=(const Matrix<T>& lhs, const Matrix<T>& rhs) {
+  return !(lhs == rhs);
 }
 
 #endif /*MATRIX_H*/
