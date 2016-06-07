0735e8d751b2e76c6b857bd1ef606bdb1b5b7630
Alexander Vogelgsang <vogel91@gmx.de>
2016-06-07 10:23:55 +0200

    B7A1a

--- Blatt07/ag_a/matmul_par.cpp
+++ Blatt07/ag_a/matmul_par.cpp
@@ -19,7 +19,6 @@ void join_all(vector<thread>& threads) {
 }
 
 struct single_threaded_tag {};
-struct single_threaded_tiled_tag {};
 struct multi_threaded_tag {};
 
 static void mult(FMatrix& c, const FMatrix& a, const FMatrix& b,
@@ -54,46 +53,32 @@ static void tile_mult(FMatrix& c, const FMatrix& a, const FMatrix& b,     //
 }
 
 static void mult(FMatrix& c, const FMatrix& a, const FMatrix& b,
-                 single_threaded_tiled_tag) {
-  const auto NN = c.height();
-  const auto MM = a.width();
-  const auto PP = c.width();
-
-  const auto tile_size = sqrt(NN);
-  assert(tile_size * tile_size == NN);
-  for (auto ii = 0u; ii < NN; ii += tile_size) {
-    const size_t N = ii + tile_size;
-    for (auto jj = 0u; jj < PP; jj += tile_size) {
-      const size_t P = jj + tile_size;
-      for (auto kk = 0u; kk < MM; kk += tile_size) {
-        const size_t M = kk + tile_size;
-        tile_mult(c, a, b, ii, jj, kk, N, P, M);
-      }
-    }
-  }
-}
-
-static void mult(FMatrix& c, const FMatrix& a, const FMatrix& b,
                  multi_threaded_tag) {
-  // const auto numthreads = 4;
+  constexpr auto numthreads = 4;
 
-  const auto NN = c.height();
-  const auto MM = a.width();
-  const auto PP = c.width();
-  const auto tile_size = cbrt(NN);
-  assert(tile_size * tile_size * tile_size == NN);
-
+  const auto N = c.height();
+  const auto M = a.width();
+  const auto P = c.width();
+  assert(("Matrices must be squre", N == M && M == P));
+  const auto tile_size = N / numthreads;
+  assert(("Matrix size must be divisible by numthreads",
+          tile_size * numthreads == N));
   vector<thread> threads;
-  for (auto ii = 0u; ii < NN; ii += tile_size*tile_size) {
-    const size_t N = ii + tile_size*tile_size;
-    for (auto jj = 0u; jj < PP; jj += tile_size*tile_size) {
-      const size_t P = jj + tile_size*tile_size;
-      for (auto kk = 0u; kk < MM; kk += tile_size*tile_size) {
-        const size_t M = kk + tile_size*tile_size;
-        threads.push_back(
-            thread{[&]() { tile_mult(c, a, b, ii, jj, kk, N, P, M); }});
+  for (auto x = 0u; x < numthreads; ++x) {
+    threads.push_back(thread{[x, &c, &a, &b, &tile_size, &M, &P]() {
+      const auto start_i = x * tile_size;
+      const auto end_i = start_i + tile_size;
+      for (auto i = start_i; i < end_i; ++i) {
+        for (auto j = 0u; j < M; ++j) {
+          // calculate c(i,j)
+          auto sum = 0.0f;
+          for (auto k = 0u; k < P; ++k) {
+            sum += a(i, k) * b(k, j);
+          }
+          c(i, j) = sum;
+        }
       }
-    }
+    }});
   }
   join_all(threads);
 }
@@ -108,22 +93,24 @@ FMatrix mult(const FMatrix& a, const FMatrix& b) {
 }
 
 void print_equal(const char* name, const FMatrix& a, const FMatrix& b) {
-  cout << name << ": " << boolalpha << (a == b) << endl;
+  constexpr auto epsilon = 0.001f;
+  bool is_equal =
+      std::equal(a.begin(), a.end(), b.begin(), b.end(),
+                 [epsilon](auto x, auto y) { return abs(x - y) < epsilon; });
+  cout << name << ": " << boolalpha << is_equal << endl;
 }
 
 int main(int argc, char* argv[]) {
-  const auto N = 64;
+  const auto N = 1024;
   FMatrix a{N, N};
   FMatrix b{N, N};
   fill_random(a, 1, 2);
   fill_random(b, 1, 2);
 
   FMatrix c_single_threaded = mult<single_threaded_tag>(a, b);
-  FMatrix c_single_threaded_tiled = mult<single_threaded_tiled_tag>(a, b);
   FMatrix c_multi_threaded = mult<multi_threaded_tag>(a, b);
 
   // cout << a << endl << b << endl;
-  print_equal("ST tiled", c_single_threaded, c_single_threaded_tiled);
   print_equal("MT tiled", c_single_threaded, c_multi_threaded);
   // cout << c_single_threaded << endl;
   return 0;
